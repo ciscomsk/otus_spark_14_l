ebc84cfe3371e2d8f529b1827ad3a3f51312c193
mkuznetsov-isg <mkuznetsov@isgneuro.com>
2021-02-22 17:50:02 +0300

    1-st commit

--- src/main/scala/ProdML.scala
+++ src/main/scala/ProdML.scala
@@ -1,41 +1,36 @@
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.{DataFrame, SaveMode}
 import org.apache.spark.ml.PipelineModel
 
-object ProdML {
-  def main(args: Array[String]): Unit = {
-    if (args.length != 3) {
-      println("Usage: ProdML <path-to-model> <path-to-input> <path-to-output>")
-      sys.exit(-1)
-    }
+import scala.util.Try
 
-    val spark = SparkSession.builder
-      .appName("ProdML")
-      .getOrCreate()
+object ProdML extends App with SparkSessionWrapper {
+  if (args.length != 3)
+    println("Usage: ProdML <path-to-model> <path-to-input> <path-to-output>")
+  sys.exit(-1)
 
-    import spark.implicits._
+  import spark.implicits._
 
-    try {
-      val model = PipelineModel.load(args(0))
+  val res: Try[Unit] = Try {
+    val model = PipelineModel.load(args(0))
 
-      val data = spark.read
-        .option("header", "true")
-        .option("inferSchema", "true")
-        .csv(args(1))
+    val data = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv(args(1))
 
-      val prediction = model.transform(data)
+    val prediction: DataFrame = model.transform(data)
 
-      prediction
-        .filter($"prediction" === 1)
-        .select("CLIENTNUM")
-        .repartition(1)
-        .write
-        .mode(SaveMode.Overwrite)
-        .csv(args(2))
+    prediction
+      .filter($"prediction" === 1)
+      .select("CLIENTNUM")
+      .repartition(1)
+      .write
+      .mode(SaveMode.Overwrite)
+      .csv(args(2))
+  }
 
-    } catch {
-      case e: Exception =>
-        println(s"ERROR: ${e.getMessage}")
-        sys.exit(-1)
-    }
+  if (res.isFailure) {
+    println(s"ERROR: $res.")
+    sys.exit(-1)
   }
 }
